49df247bc368e368c5fbab63f8700a9562ea722a
Sam <sambusinesstransac@gmail.com>
2023-02-07 06:46:27 +0800

    fix: review the code

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">Order by Title</button>
+        <button @click="handleClick('location')">Order by Location</button>
+        <button @click="handleClick('salary')">Order by Salary</button>
+      </div>
+    </header>
+    <JobList :jobs="jobs" :order="order" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, reactive, toRefs, ref } from "vue";
+import Job from "@/types/Jobs";
+import JobList from "@/components/JobList.vue";
+import OrderTerm from "@/types/OrderTerm";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: { JobList },
+  setup() {
+    const jobs = ref<Job[]>([
+      {
+        title: "Farm worker",
+        location: "lon lon ranch",
+        salary: 2000,
+        id: "1",
+      },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      { title: "fisherman", location: "lake hyliaaa", salary: 21000, id: "4" },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
+    const order = ref<OrderTerm>("title"); // get the valid types in the OrderTerm
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+    return { jobs, handleClick, order };
+  },
+  methods: {},
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+header {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
 }
 </style>
